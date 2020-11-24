53d19834114451b6102be754bcd5d2ee7bf53f57
jongwanKim <45841674+jongwankim0317@users.noreply.github.com>
2020-11-24 02:19:48 +0000

    Update char_rnn.py

--- char_rnn.py
+++ char_rnn.py
@@ -1,19 +1,27 @@
+"""
+* Copyright 2020 Jongwankim
+* Released under the MIT license.
+* http://github.com/jongwankim0317
+"""
+
 import torch
 import torch.nn as nn
 
 class RNN(nn.Module):
+
     def __init__(self, input_size, hidden_size, output_size):
         super(RNN, self).__init__()
-        
-        ##### please implment here
-        
+
+        self.hidden_size = hidden_size
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
 
         self.softmax = nn.LogSoftmax(dim=1)
 
-    def forward(self, d, hidden):
-        
-        ##### please implment here
-        
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
         output = self.softmax(output)
-        return output
 
+        return output
