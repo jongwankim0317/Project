784433e16d05d59f9252626863d6d21cec51c0ad
jongwanKim <45841674+jongwankim0317@users.noreply.github.com>
2020-11-27 04:53:09 +0000

    Update Basic CNN pytorch.py

--- Basic/Basic CNN pytorch.py
+++ Basic/Basic CNN pytorch.py
@@ -10,7 +10,7 @@ from glob import glob
 seed = 1
 batch_size = 4096
 test_batch_size = 4096
-epoch = 1
+epoch = 50
 
 GPU_NUM = 0
 device = torch.device(f'cuda:{GPU_NUM}' if torch.cuda.is_available() else 'cpu')
@@ -94,3 +94,23 @@ for epoch in range(1, epoch+1):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                        100. * batch_idx / len(train_loader), loss.item()))
+
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+
+            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average Loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset), 100.*correct/len(test_loader.dataset)
+    ))
