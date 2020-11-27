1539c417d8f64253413c925508a8a821705fc580
jongwanKim <45841674+jongwankim0317@users.noreply.github.com>
2020-11-27 09:54:21 +0900

    Add files via upload

---
+++ Basic/basic pytorch.py
@@ -0,0 +1,96 @@
+import torch
+from torchvision import datasets, transforms
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.optim as optim
+import os
+from glob import glob
+
+
+seed = 1
+batch_size = 4096
+test_batch_size = 4096
+epoch = 1
+
+GPU_NUM = 0
+device = torch.device(f'cuda:{GPU_NUM}' if torch.cuda.is_available() else 'cpu')
+torch.cuda.set_device(device) # change allocation of current GPU
+print ('Current cuda device ', torch.cuda.current_device()) # check
+
+# Additional Infos
+if device.type == 'cuda':
+    print(torch.cuda.get_device_name(GPU_NUM))
+    print('Memory Usage:')
+    print('Allocated:', round(torch.cuda.memory_allocated(GPU_NUM)/1024**3,1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_cached(GPU_NUM)/1024**3,1), 'GB')
+
+torch.manual_seed(seed)
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('dataset/', train=True, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize(mean=(0.5, ), std=(0.5,))
+                   ])),
+    batch_size=batch_size,
+    shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('dataset', train=False,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.5,),(0.5))
+                   ])),
+    batch_size=test_batch_size,
+    shuffle=True)
+
+# MODEL
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+
+        # print(x.shape) #(for size)
+        x = x.view(-1, 4*4*50) # batch size: -1 (don't know)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+model = Net().to(device)
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.5)
+
+param = list(model.parameters())
+for i in range(8):
+    print(param[i].size())
+
+data, target = next(iter(train_loader))
+print(data.shape, target.shape)
+
+log_interval = 100
+
+for epoch in range(1, epoch+1):
+    model.train()
+
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+
+        optimizer.step()
+
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.item()))
